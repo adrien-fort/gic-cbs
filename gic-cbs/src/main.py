733b0e0a892d131588f00a9beda838a5822f65bf
Adrien Fort <fort.adrien@gmail.com>
2025-09-17 18:44:26 +0800

    Add .gitignore and update main.py

--- gic-cbs/src/main.py
+++ gic-cbs/src/main.py
@@ -1,5 +1,6 @@
 def main():
     print("Welcome to the GIC CBS application!")
+    print("Please define movie title and seating map in [Title] [Row] [SeatsPerRow] format:")
 
 if __name__ == "__main__":
     main()
